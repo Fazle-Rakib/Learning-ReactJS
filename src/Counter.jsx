9784de12ce69dc13f5aa77dce65da652036a31d4
Fazle-Rakib <fazle30@student.sust.edu>
2025-10-08 19:04:59 +0600

    Updater function with useState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -6,15 +6,30 @@ function Counter() {
     const [count, setCount] = useState(0);
 
     const decrementCount = () =>{
-        setCount(count-1);
+        // setCount(count-1);
+        // setCount(count-1);
+        // setCount(count-1);
+
+        // Updater function
+        setCount(prevCount => prevCount - 1);
+        setCount(prevCount => prevCount - 1);
+        setCount(prevCount => prevCount - 1);
     }
 
     const resetCount = () =>{
-        setCount(0);
+        // setCount(0);
+        setCount(c => c = 0);
     }
 
     const incrementCount = () =>{
-        setCount(count+1);
+        // setCount(count+1);
+        // setCount(count+1);
+        // setCount(count+1);
+
+        // Updater function
+        setCount(prevCount => prevCount + 1);
+        setCount(prevCount => prevCount + 1);
+        setCount(prevCount => prevCount + 1);
     }
 
     return(
